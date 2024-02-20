3169c88c5afd4d26b92b083b6dd08dc913aa5552
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2024-02-20 12:27:49 -0300

    dificultando ex0_idor

--- 3_vulnerabilidades2/ex0_idor/app.py
+++ 3_vulnerabilidades2/ex0_idor/app.py
@@ -1,12 +1,17 @@
 # app.py
 
+import base64
 from flask import Flask, render_template, redirect, url_for, request
+
+
+def encrypt_base64(text):
+    return base64.b64encode(text.encode('utf-8')).decode('utf-8')
 
 app = Flask(__name__)
 
 # Nota
 class Nota:
-    def __init__(self, id, titulo, conteudo, autor: str = "myself"):
+    def __init__(self, id, titulo, conteudo, autor = "myself"):
         self.id = id
         self.titulo = titulo
         self.conteudo = conteudo
@@ -22,16 +27,16 @@ class Nota:
 
 # Lista de notas (simulada)
 notas = [
-    Nota(0, "hacked.txt", "Parabéns, você concluiu o exercício de IDOR 🚪!", "admin"),
-    Nota(1, "todo.txt", "CTF do THM chamado BoilerCTF, Continuar a IC e Ir na academia", "caiogomes"),
-    Nota(2, "Tarefa hacker", "Tente acessar uma nota do perfil do administrador"),
+    Nota("YWRtaW4w", "hacked.txt", "Parabéns, você concluiu o exercício de IDOR 🚪!", "admin"),
+    Nota("Y2Fpb2dvbWVzMA==", "todo.txt", "CTF do THM chamado BoilerCTF, Continuar a IC e Ir na academia", "caiogomes"),
+    Nota("bXlzZWxmMA==", "Tarefa hacker", "Tente acessar uma nota do perfil do administrador", "myself"),
 ]
 
 @app.route('/')
 def index():
     return render_template('index.html', notas=[nota.to_dict() for nota in notas])
 
-@app.route('/nota/<int:nota_id>')
+@app.route('/nota/<string:nota_id>')
 def ver_nota(nota_id):
     nota = next((nota for nota in notas if nota.id == nota_id), None)
     if nota:
@@ -43,7 +48,7 @@ def criar_nota():
     if request.method == 'POST':
         titulo = request.form['titulo']
         conteudo = request.form['conteudo']
-        nova_nota = Nota(len(notas), titulo, conteudo)
+        nova_nota = Nota(encrypt_base64(f"myself{len(notas)-2}"), titulo, conteudo, "myself")
         notas.append(nova_nota)
         return redirect(url_for('index'))
     return render_template('criar_nota.html')
