de2f27bcdbbb24d83446ce5d50ace3a838e1b482
João Vitor <21.01075-7@maua.br>
2024-02-21 14:46:32 -0300

    ex0_lfi created

--- 3_vulnerabilidades2/ex0_lfi/app.py
+++ 3_vulnerabilidades2/ex0_lfi/app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, send_from_directory, redirect, url_for, request
 import os
 
+def path_adaptation(full_path, path_to_find):
+    change_back_count = path_to_find.count('../')
+    if change_back_count == 0:
+        return full_path + '/' + path_to_find
+    full_path = full_path.split('/')
+    full_path = full_path[:-change_back_count]
+    full_path = '/'.join(full_path) + '/' + path_to_find.replace("../", "")
+    return full_path
+
+def filter_query(path):
+    return path.replace("../", "")
 
 app = Flask(__name__)
-path = os.path.dirname(os.path.abspath(__file__)) + '\\livros'
+path = f'{os.path.dirname(os.path.abspath(__file__))}\\livros'.replace("\\", "/")
 
 @app.route('/')
 def index():
@@ -12,9 +23,18 @@ def index():
 
 @app.route('/livros')
 def ler_livro():
+    print()
     livro = request.args.get('livro')
     if livro:
-        return send_from_directory('livros', livro)
+        content = ""
+        try:
+            path_param = filter_query(livro)
+            with open(path_adaptation(path, path_param), 'r') as f:
+                content = f.read()
+            return content
+        except:
+            return "Book not found"
+
     else:
         return redirect(url_for('index'))
 
